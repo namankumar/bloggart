d4c402cd5d8df1f62b1f5b8fa3f4779da1be3d3f
naman <ping@namank.com>
2012-06-27 23:50:34 -0400

    updated current theme

---
+++ themes/default/static/js/site.js
@@ -0,0 +1,10 @@
+function toggle(id) {
+    var ele = document.getElementById("toggleText");
+    var text = document.getElementById("displayText");
+    if(ele.style.display == "block") {
+	ele.style.display = "none";
+    }
+    else {
+	ele.style.display = "block";
+    }
+} 
